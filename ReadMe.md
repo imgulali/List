c32b9cd0720a3c326238a20ee5d5874743df22c3
imgulali <gulali.official@gmail.com>
2024-05-14 23:24:13 +0500

    Update

--- ReadMe.md
+++
@@ -1,5 +0,0 @@
-
-# List
-
-List is an easy to use app for listing and shring items in JavaScript, with an update handling server.
-
--- server/src/middlewares/ErrorHandler.js
+++
@@ -1,7 +0,0 @@
-export const ErrorHandler = (err, req, res, next) => {
-    console.error(err.stack);
-    const status = err.status || 501;
-    const message = "Internal Server Error";
-    const error = err.message || "An unxpected error occured";
-    return res.error(message, error, status)
-};
